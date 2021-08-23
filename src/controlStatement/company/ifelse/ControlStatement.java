f0221fee73aece9a8edadcf15afdbcc847f849ad
thahirsmd9063 <thahirsmd@gmail.com>
2021-08-23 19:41:55 +0530

    hello statement ifelse

--- src/controlStatement/company/ifelse/ControlStatement.java
+++ src/controlStatement/company/ifelse/ControlStatement.java
@@ -3,6 +3,15 @@ package controlStatement.company.ifelse;
 public class ControlStatement {
 	public static void main(String []args) {
 		System.out.println("welcome control system");
+		int value7 = 250;
+		int value8 = 350;
+		if(value7 >value8) {
+			System.out.println(value7);
+		}
+		else {
+			System.out.println(value8);
+		}
+		
 	}
 
 }
